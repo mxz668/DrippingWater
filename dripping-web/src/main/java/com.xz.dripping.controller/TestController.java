df82f221f7d2c33b9340617d0404c7750fed79eb
MABIAO <mabiao@zillionfortune.com>
2017-06-05 12:23:54 +0800

    Project init

---
+++ dripping-web/src/main/java/com.xz.dripping.controller/TestController.java
@@ -0,0 +1,24 @@
+package com.xz.dripping.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by MABIAO on 2017/6/2 0002.
+ */
+@RestController
+@RequestMapping("testController")
+public class TestController {
+
+    Logger logger = LoggerFactory.getLogger(TestController.class);
+
+    @RequestMapping(value = "test",method = RequestMethod.POST)
+    public void test(){
+        int i = 1;
+        System.out.println(i);
+        logger.info("hah");
+    }
+}
