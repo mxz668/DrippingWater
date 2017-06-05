9721da7f24aed12a1825f0a1caf2421f4a0a2b68
MABIAO <mabiao@zillionfortune.com>
2017-06-05 13:56:57 +0800

    spingmvc

---
+++ dripping-service/src/main/java/com/xz/service/ServiceTest.java
@@ -0,0 +1,7 @@
+package com.xz.service;
+
+/**
+ * Created by MABIAO on 2017/6/2 0002.
+ */
+public class ServiceTest {
+}
--- dripping-web/src/main/java/com/xz/controller/TestController.java
+++ dripping-web/src/main/java/com/xz/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.xz.dripping.controller;
+package com.xz.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
